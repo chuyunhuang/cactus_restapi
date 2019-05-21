9d1ea10d7281b759aa2f29c054ed32ec8414e6e5
Chuyun Huang <chuyunhuang@pursuit.org>
2019-05-21 16:31:43 -0400

    fix db

--- app.js
+++ app.js
@@ -17,27 +17,6 @@ app.get("/test", (req, res) => {
   res.send("Server working!");
 });
 
-// app.use("/user", userRouter);
-
-app.get("/user", (req, res) => {
-  const DATABASE_URL =
-    "postgres://njietocmnfthvb:5c7200683a734bfaed915bf10598908bdf61912976bbfd976b08a729ffa602bd@ec2-54-225-95-183.compute-1.amazonaws.com:5432/da2svtmj4bj3ac";
-  const { Client } = require("pg");
-
-  const db = new Client({
-    connectionString: DATABASE_URL,
-    ssl: true
-  });
-
-  db.connect();
-
-  db.query("SELECT * FROM users", (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    db.end();
-  });
-});
+app.use("/user", userRouter);
 
 module.exports = { app };
--- services/herokuDB.js
+++ services/herokuDB.js
@@ -1,9 +1,7 @@
-const DATABASE_URL =
-  "postgres://njietocmnfthvb:5c7200683a734bfaed915bf10598908bdf61912976bbfd976b08a729ffa602bd@ec2-54-225-95-183.compute-1.amazonaws.com:5432/da2svtmj4bj3ac";
 const { Client } = require("pg");
 
 const db = new Client({
-  connectionString: DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
