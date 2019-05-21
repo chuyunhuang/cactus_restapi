975e1e3f346569a945b5d6a97e946116c25970c3
Chuyun Huang <chuyunhuang@pursuit.org>
2019-05-21 15:20:29 -0400

    testing heroku db connect

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -1,4 +1,5 @@
-const pgp = require("pg-promise")({});
-const db = pgp("postgres://localhost/cactus2");
+// const pgp = require("pg-promise")({});
+// const db = pgp("postgres://localhost/cactus2");
+const db = require("../services/herokuDB");
 
 module.exports = db;
