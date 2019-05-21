d9e6a00c4358c4e58d5e57387cca195ee3a36ea3
Chuyun Huang <chuyunhuang@pursuit.org>
2019-05-21 16:37:32 -0400

    fix db

--- services/dbConnection.js
+++ services/dbConnection.js
@@ -1,5 +1,4 @@
 const pgp = require("pg-promise")({});
-const db = pgp("postgres://localhost/cactus");
-// const db = require("./herokuDB");
+const db = pgp(process.env.DATABASE_URL || "postgres://localhost/cactus");
 
 module.exports = db;
